7c00a00d67409e1aba8b7c048e80f3c5023c30ce
ilhamsj <ilhamsaputrajati@gmail.com>
2026-02-02 10:29:50 +0700

    feat: add `afterChange` hook to campaigns collection to trigger email sending

--- src/payload/collections/campaigns/hooks.ts
+++ src/payload/collections/campaigns/hooks.ts
@@ -1,5 +1,31 @@
-import { CollectionConfig } from 'payload'
+import { Campaign } from '@/payload/payload-types'
+import { CollectionAfterChangeHook, CollectionConfig } from 'payload'
+
+const triggerEmailSending: CollectionAfterChangeHook<Campaign> = async (args) => {
+  const { previousDoc, doc, operation, req } = args
+  const { payload } = req
+
+  const template = await payload
+    .findByID({
+      collection: 'templates',
+      id: typeof doc.content.template === 'string' ? doc.content.template : doc.content.template.id,
+    })
+    .catch(() => {
+      payload.logger.info({ msg: 'Template not found', templateId: doc.content.template })
+    })
+
+  if (!template) return
+
+  const email = await payload.sendEmail({
+    to: 'test@example.com',
+    subject: doc.subject,
+    text: doc.subject,
+    html: template.html,
+  })
+
+  payload.logger.info({ msg: 'Email sent', email, doc })
+}
 
 export const hooks: CollectionConfig['hooks'] = {
-  afterChange: [],
+  afterChange: [triggerEmailSending],
 }
